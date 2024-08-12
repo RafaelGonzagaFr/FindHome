fa055719f841c287ee6f4d6292b0d738630f6d2d
Rafael <rafaelgonzag4@gmail.com>
2024-08-12 06:51:50 -0300

    Feat. Alteração de repositories para inserir segurança

--- findhome/src/main/java/gtech/findhome/domain/repositories/OwnerRepository.java
+++ findhome/src/main/java/gtech/findhome/domain/repositories/OwnerRepository.java
@@ -3,5 +3,8 @@ package gtech.findhome.domain.repositories;
 import gtech.findhome.domain.entities.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OwnerRepository extends JpaRepository<Owner, String> {
+    Optional<Owner> findByLogin(String login);
 }
--- findhome/src/main/java/gtech/findhome/domain/repositories/TenantRepository.java
+++ findhome/src/main/java/gtech/findhome/domain/repositories/TenantRepository.java
@@ -3,5 +3,9 @@ package gtech.findhome.domain.repositories;
 import gtech.findhome.domain.entities.Tenant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TenantRepository extends JpaRepository<Tenant, String> {
+    Optional<Tenant> findByLogin(String login);
+
 }
